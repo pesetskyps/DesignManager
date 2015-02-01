f71364d3cdea8ebc4c6e8686be99593b59bf513e
pesetskyps <pesetskyps@gmail.com>
2015-02-01 13:43:54 +0300

    scrollable panel ready

---
+++ Assets/Scripts/ScrollingList/ListPopulator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item{
+	public string name;
+	public string type;
+	public string rarity;
+	public Sprite icon;
+	public bool isChampion;
+}
+
+public class ListPopulator : MonoBehaviour {
+
+	public GameObject prefabButton;
+	public List<Item> itemList;
+	public Transform contentPanel;
+	void Start(){
+		PopulateList ();
+	}
+
+	void PopulateList ()
+	{
+		foreach (var item in itemList) {
+			GameObject newButton = Instantiate(prefabButton) as GameObject;	
+			SampleButton sampleobj = newButton.GetComponent<SampleButton>();
+			sampleobj.nameLabel.text = item.name;
+			sampleobj.typeLabel.text = item.type;
+			sampleobj.rarityLabel.text = item.rarity;
+			sampleobj.icon.sprite = item.icon;
+			sampleobj.isChampionIcon.SetActive(item.isChampion);
+
+			newButton.transform.SetParent(contentPanel,false);
+			contentPanel.SetAsLastSibling();
+		}
+	}
+}
---
+++ Assets/Scripts/ScrollingList/SampleButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+	public Button button;
+	public Text nameLabel;
+	public Text typeLabel;
+	public Text rarityLabel;
+	public Image icon;
+	public GameObject isChampionIcon;
+}
+
+
+
