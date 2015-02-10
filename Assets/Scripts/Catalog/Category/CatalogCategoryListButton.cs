85147b099f0db3d6cf308f8f4e6462d95a1476c6
pesetskyps <pesetskyps@gmail.com>
2015-02-10 15:57:24 +0300

    temp

--- Assets/Scripts/Catalog/Category/CatalogCategoryListButton.cs
+++ Assets/Scripts/Catalog/Category/CatalogCategoryListButton.cs
@@ -2,15 +2,15 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CatalogCategoryListButton : MonoBehaviour, IPointerDownHandler//, IPointerEnterHandler
+public class CatalogCategoryListButton : MonoBehaviour//, IPointerDownHandler//, IPointerEnterHandler
 {
     public Button button;
     public CatalogSectionName catalogType;
     public Text nameLabel;
     public Image icon;
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("hee");
-    }
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    Debug.Log("hee");
+    //}
 }
