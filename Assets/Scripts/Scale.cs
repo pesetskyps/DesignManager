d45daab6bec5a02d1aad4a26b14147deea8c3ff0
pesetskyps <pesetskyps@gmail.com>
2015-03-03 19:17:14 +0300

    merge conflict solve

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Scale : MonoBehaviour
-{
-    void Start()
-    {
+public class Scale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
         transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-    }
+	}
 
 }
